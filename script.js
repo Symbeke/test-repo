1de643d13ab417a42ae14e9e9e257ee7c70910f4
Кумаров Сымбат Кайратулы <@itstep.loc>
2022-10-18 21:13:08 +0600

    task with folders done

---
+++ script.js
@@ -0,0 +1,39 @@
+let arr =  [
+{
+    name:"Folder 1",
+    folders:[
+        {name:"subfolder 1"},
+        {name:"subfolder 2"},
+        {name:"subfolder 3"},
+    ]
+},
+{
+    name:"Folder 2",
+    folders:[
+        {name:"subfolder 4"},
+        {name:"subfolder 5"},
+        {name:"subfolder 6"},
+    ]
+},
+{
+    name:"Folder 3",
+    folders:[
+        {name:"subfolder 7"},
+        {name:"subfolder 8"},
+        {name:"subfolder 9"},
+    ]
+},
+]
+
+
+function showParent(subfolderName){
+    for (let folder of arr){
+        for(let subfolder of folder.folders){
+            if(subfolder.name === subfolderName){
+                console.log(folder.name);
+                break;
+            }
+        }
+    }
+}
+showParent("subfolder 1");
